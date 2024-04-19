22f50a3ac1e9da56f1d903968c8104b334c34e58
TechStudent10 <76978184+TechStudent10@users.noreply.github.com>
2024-04-19 16:32:05 -0600

    alphalaneous suggestions

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ class $modify(CCMenuItemSpriteExtra) {
 	void activate() {
 		CCMenuItemSpriteExtra::activate();
 		auto sfxFile = Mod::get()->getSettingValue<ghc::filesystem::path>("sfx");
-		log::info("file: {}, exists: {}", sfxFile, ghc::filesystem::exists(sfxFile));
+		// log::info("file: {}, exists: {}", sfxFile, ghc::filesystem::exists(sfxFile));
 		if (sfxFile == "") return;
 		if (!Mod::get()->getSettingValue<bool>("in-editor") && (GameManager::sharedState()->getEditorLayer() != nullptr)) return;
 
